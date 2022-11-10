3dfe501ef93b29ab3c0942da44539b87c175b734
Anton Kritskiy <antonkritskiy@Antons-MacBook-Pro.local>
2022-11-10 12:29:32 -0800

    Completed Entry & Index Page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse, HttpResponseNotFound
+import markdown2
+
 
 from . import util
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
+
     })
+
+def page(request, title):
+    entryText = util.get_entry(title)
+    for page in util.list_entries():
+        if page == title:
+            return render(request, "encyclopedia/entry.html", {
+                "title": title,
+                "entry": markdown2.markdown(entryText)
+            })
+    return HttpResponseNotFound('<h1> Entry not found </h1>')
+
 
