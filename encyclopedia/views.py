9bafc07f70673fdba074e9a5b3b45f50bb11f4c1
Anton Kritskiy <antonkritskiy@Antons-MacBook-Pro.local>
2022-11-11 14:46:24 -0800

    Completed Search

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -5,9 +5,17 @@ import markdown2
 
 from . import util
 
+def SearchSublist(text):
+    entries = util.list_entries()
+    newEntries = []
+    for entry in entries:
+        if text in entry:
+            newEntries.append(entry)
+    return newEntries
+
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {
+        return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries(),
 
     })
@@ -22,4 +30,16 @@ def page(request, title):
             })
     return HttpResponseNotFound('<h1> Entry not found </h1>')
 
+
+def pageSearch(request):
+    entryText = util.get_entry(request.GET.get('q'))
+    for page in util.list_entries():
+        if page == request.GET.get('q'):
+            return render(request, "encyclopedia/entry.html", {
+                "title": request.GET.get('q'),
+                "entry": markdown2.markdown(entryText)
+            })
+    return render(request,"encyclopedia/search.html", {
+        "entries": SearchSublist(request.GET.get('q'))
+    })
 
