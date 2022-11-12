b1ae982c7bc9acb850576338769ee6df370c38c5
Anton Kritskiy <antonkritskiy@Antons-MacBook-Pro.local>
2022-11-11 18:40:04 -0800

    Added Create New Page

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.page, name="page"),
     path("?q", views.pageSearch, name="search"),
+    path("new", views.pageNew, name="new"),
+    path("saved", views.entrySave, name="save"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -9,9 +9,12 @@ def SearchSublist(text):
     entries = util.list_entries()
     newEntries = []
     for entry in entries:
-        if text in entry:
+        if text in entry or text.capitalize() in entry or text.upper() in entry:
             newEntries.append(entry)
-    return newEntries
+    if not len(newEntries) == 0:
+        return newEntries
+    else:
+        return None
 
 
 def index(request):
@@ -32,14 +35,42 @@ def page(request, title):
 
 
 def pageSearch(request):
-    entryText = util.get_entry(request.GET.get('q'))
+    q =  request.GET.get('q')
     for page in util.list_entries():
-        if page == request.GET.get('q'):
+        if page == q:
             return render(request, "encyclopedia/entry.html", {
-                "title": request.GET.get('q'),
-                "entry": markdown2.markdown(entryText)
+                "title": q,
+                "entry": markdown2.markdown(util.get_entry(q))
             })
+        elif page == q.upper():
+            return render(request, "encyclopedia/entry.html", {
+            "title": q.upper(),
+            "entry": markdown2.markdown(util.get_entry(q.upper()))
+            })
+        elif page == q.capitalize():
+            return render(request, "encyclopedia/entry.html", {
+            "title": q.capitalize(),
+            "entry": markdown2.markdown(util.get_entry(q.capitalize()))
+            })
+
     return render(request,"encyclopedia/search.html", {
         "entries": SearchSublist(request.GET.get('q'))
+    })
+
+def pageNew(request):
+    return render(request,"encyclopedia/new.html")
+
+def entrySave(request):
+    entryText = request.GET.get('newEntry')
+    if entryText.split()[0] == "#":
+        entryName = entryText.split()[1]
+    elif entryText.split()[0][0] == "#":
+        entryName = entryText.split()[0][1:]
+    else:
+        entryName = entryText.split()[0]
+    util.save_entry(entryName, entryText)
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries(),
+
     })
 
